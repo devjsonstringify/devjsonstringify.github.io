9bacf1ce63ae9d9c55888ef3f8cd9719f81bd7c2
Glen Mark Cabiladas <gcabiladas@gmail.com>
2019-04-24 10:55:29 +0800

    Add typewriter style for key skill

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const instance = new TypeIt('#js-typeWriter', {
+	strings: [
+		'HTML4/5',
+		'CSS2/3',
+		'JavaScript',
+		'Jquery',
+		'React',
+		'Router',
+		'Sass',
+		'Php',
+		'MySql',
+		'Joomla',
+		'WordPress',
+		'Linux scripting',
+		'Web API',
+		'Browser API',
+		'RESTful services',
+		'Ajax',
+		'Webpack',
+		'Version Control',
+		'NPM',
+		'Chrome/Firefox  Developer tools'
+	],
+	speed: 50,
+	breakLines: false,
+	waitUntilVisible: true,
+	loop: true
+}).go()
